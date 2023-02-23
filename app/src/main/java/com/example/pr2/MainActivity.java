eeb34a7324ad5391c4cce76e295ef0f8aec11c9a
Evelina <eve1ina@list.ru>
2023-02-23 18:34:23 +0300

    Проинициализированы кнопка и текст на главном экране, добавлено логирование нажатия на кнопку

--- app/src/main/java/com/example/pr2/MainActivity.java
+++ app/src/main/java/com/example/pr2/MainActivity.java
@@ -2,42 +2,58 @@ package com.example.pr2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
+    private Button add_car;
+    private TextView add_car_text;
+    private ImageView icon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        add_car_text = findViewById(R.id.add_car_text);
+        add_car = findViewById(R.id.add_car_button);
+        icon = findViewById(R.id.Car);
+
+        icon.setImageResource(R.drawable.car);
+        add_car_text.setText(R.string.car_is_not_added);
+
+
+    }
+    public void ButtonClicked(View view){
+        Log.d(TAG, "Нажата кнопка Добавить автомобиль");
     }
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onStart");
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onPause");
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onRestart");
     }
 
@@ -45,13 +61,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onStop");
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onDestroy");
     }
 }
