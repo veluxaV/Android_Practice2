e4ffa0e5c9e71d81537d4dad0fdeab794372b45c
Evelina <eve1ina@list.ru>
2023-03-03 00:25:25 +0300

    Создан Fragment "Add_car"

---
+++ app/src/main/java/com/example/pr2/Add_car.java
@@ -0,0 +1,19 @@
+package com.example.pr2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Add_car extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_car, container, false);
+    }
+}
